8297dcc39c9985dc77db9f4a9fcbb8a6de5399ca
Eugene <openmind1377@gmail.com>
2020-06-16 15:22:40 +0300

    Updates

---
+++ precache-manifest.3d8289549ff1311a395bc52b14e74cea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf120279dc8444e625b3c278e9f0d2e",
+    "url": "/asales/index.html"
+  },
+  {
+    "revision": "8b0f8139fbf4893b9210",
+    "url": "/asales/static/css/main.13694515.chunk.css"
+  },
+  {
+    "revision": "7481759cda1dfd6fe48c",
+    "url": "/asales/static/js/2.9a886f28.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/asales/static/js/2.9a886f28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b0f8139fbf4893b9210",
+    "url": "/asales/static/js/main.9c2210b6.chunk.js"
+  },
+  {
+    "revision": "c3a337f6e9c0bf0388c9",
+    "url": "/asales/static/js/runtime-main.f6181f82.js"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/asales/static/media/aviasales_logo.0c089234.svg"
+  },
+  {
+    "revision": "68ca57dc522eda771a31bb07bdd256b1",
+    "url": "/asales/static/media/checkmark.68ca57dc.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aviasales20/precache-manifest.4634dfbd3512c7fac35ca145ffeb0821.js"
+  "/asales/precache-manifest.3d8289549ff1311a395bc52b14e74cea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aviasales20/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/asales/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
